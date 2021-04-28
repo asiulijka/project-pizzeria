83612c106972ab2e7fe30d20baa15d31599f9a7a
Joanna Zielinska <j.m.h.zielinska@gmail.com>
2021-04-29 01:26:36 +1000

    Add class - Product in JS

--- src/js/script.js
+++ src/js/script.js
@@ -52,7 +52,59 @@
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
   };
 
+
+  class Product{
+    constructor(id, data){
+      const thisProduct = this;
+
+      thisProduct.id = id;
+      thisProduct.data = data;
+
+      thisProduct.renderInMenu();
+
+      console.log('newProduct:', thisProduct);
+    }
+
+    renderInMenu(){
+      const thisProduct = this;
+
+      // generate html based on template
+      const generatedHTML = templates.menuProduct(thisProduct.data);
+
+      // create element using utils.createElementFromHTML
+      thisProduct.element = utils.createDOMFromHTML(generatedHTML);
+
+      // find menu container
+      const menuContainer = document.querySelector(select.containerOf.menu);
+
+      // add element to menu
+      menuContainer.appendChild(thisProduct.element);
+
+
+    }
+
+  }
+
   const app = {
+    initMenu: function(){
+      const thisApp = this;
+
+      console.log('thisApp.data:', thisApp.data);
+      //
+      // const testProduct = new Product();
+      // console.log('testProduct:',testProduct);
+      for(let productData in thisApp.data.products){
+        new Product(productData, thisApp.data.products[productData]);
+      }
+
+    },
+
+    initData: function(){
+      const thisApp = this;
+
+      thisApp.data = dataSource;
+    },
+
     init: function(){
       const thisApp = this;
       console.log('*** App starting ***');
@@ -60,8 +112,17 @@
       console.log('classNames:', classNames);
       console.log('settings:', settings);
       console.log('templates:', templates);
+
+      thisApp.initData();
+      thisApp.initMenu();
     },
   };
 
   app.init();
+  app.initMenu();
+
+
+
+
+
 }
